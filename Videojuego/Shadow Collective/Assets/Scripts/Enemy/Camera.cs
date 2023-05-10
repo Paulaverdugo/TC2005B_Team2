8afde7e754e4ea72092f50c20fbbb25058674e32
coldhed <santiago.rdgz2001@gmail.com>
2023-05-10 10:31:36 -0600

    Created the script for the Camera enemy

---
+++ Videojuego/Shadow Collective/Assets/Scripts/Enemy/Camera.cs
@@ -0,0 +1,16 @@
+/*
+    Script that defines the behavior for the enemy camera
+
+    This class inherits from the abstract class Base Enemy
+*/
+
+
+// from Assets.Scripts.Abstract import BaseEnemy;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : BaseEnemy
+{
+
+}
