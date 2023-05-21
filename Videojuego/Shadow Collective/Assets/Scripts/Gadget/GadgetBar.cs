4400f99180294c63e041fb0030ee502d7c735fab
Paula Verdugo <paulaverdugo@Paulas-MacBook-Pro-2.local>
2023-05-21 14:48:24 -0600

    Adding gadget inventory and How to play instructions to game page

---
+++ Videojuego/Shadow Collective/Assets/Scripts/Gadget/GadgetBar.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GadgetBar : MonoBehaviour
+{
+    public bool[] isFull;
+    public GameObject[] gadget;
+}
---
+++ Videojuego/Shadow Collective/Assets/Scripts/Gadget/GetGadget.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetGadget : MonoBehaviour
+{
+//    private Inventory inventory;
+
+//    private void Start(){
+//          inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+//    }
+
+//    void OnTriggerEnter2D(Collider2D other){
+//     if(other.CompareTag("Player"))
+//     {
+//         for(int i = 0; i < inventory.slots.Length; i++)
+//         {
+//             if(inventory.isFull[i] == false)
+//             {
+//                 // Se puede recoger el gadget y se instancia en el inventario
+//                 inventory.isFull[i] = true;
+//                 Instantiate(gadget, inventory.slots[i].transform, false);
+//                 Destroy(gameObject);
+//                 break;
+//             }
+//         }   
+//    }
+//    }
+}
+
+// Para agregar funcionalidad pueden checar https://youtu.be/DLAIYSMYy2g?t=307
