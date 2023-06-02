614dd97f22a32f7c67c11253a3104bdeed21fa87
coldhed <santiago.rdgz2001@gmail.com>
2023-06-02 14:45:22 -0600

    User can now log in

--- Videojuego/Shadow Collective/Assets/Scripts/API/ApiConstants.cs
+++ Videojuego/Shadow Collective/Assets/Scripts/API/ApiConstants.cs
@@ -1,12 +1,16 @@
 /*
     Class to have all of the API constants, such as the URI and the Port in one place
 */
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public static class ApiConstants
 {
     public static string URL = "http://localhost:8000";
 }
 
+[System.Serializable]
 public class User
 {
     public string user_name;
@@ -14,4 +18,17 @@ public class User
     public string email;
     public string age;
     // the User class also haves a timestamp
+}
+
+[System.Serializable]
+public class ShortUser
+{
+    public string user_name;
+    public string user_password;
+}
+
+[System.Serializable]
+public class ShortUserList
+{
+    public List<ShortUser> users;
 }
--- Videojuego/Shadow Collective/Assets/Scripts/API/Login.cs
+++ Videojuego/Shadow Collective/Assets/Scripts/API/Login.cs
@@ -1,36 +1,69 @@
-// /*
-//     Script that belongs to the log in button
+/*
+    Script that belongs to the log in button
 
-//     Takes in a username and password and checks if it exists 
-// */
+    Takes in a username and password and checks if it exists 
+*/
 
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using TMPro;
-// using UnityEngine.Networking;
-// using UnityEditor;
-// using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Networking;
+using UnityEditor;
+using UnityEngine.SceneManagement;
 
-// public class Login : MonoBehaviour
-// {
-//     [SerializeField] private TMP_InputField username;
-//     [SerializeField] private TMP_InputField password;
-//     [SerializeField] private GameObject errorMessage;
+public class Login : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField username;
+    [SerializeField] private TMP_InputField password;
+    [SerializeField] private GameObject errorMessage;
 
 
-//     void Start()
-//     {
-//         gameObject.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(TaskOnClick);
-//     }
+    void Start()
+    {
+        gameObject.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(TaskOnClick);
+    }
 
-//     void TaskOnClick()
-//     {
-        
-//     }
+    void TaskOnClick()
+    {
+        StartCoroutine(LogIn());
+    }
 
-//     private IEnumerator LogIn()
-//     {
+    private IEnumerator LogIn()
+    {
+        string ep = ApiConstants.URL + "/users/" + username.text;
 
-//     }
-// }
+        using (UnityWebRequest www = UnityWebRequest.Get(ep))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.Success) 
+            {
+                // the response is a list of json short users
+                if (www.downloadHandler.text != "[]")
+                {
+                    string jsonString = "{\"users\":" + www.downloadHandler.text + "}";
+                    ShortUserList shortUserList = JsonUtility.FromJson<ShortUserList>(jsonString);
+                    ShortUser shortUser = shortUserList.users[0];
+
+                    // if the password returns matches the password inputed, we are good to go
+                    if (shortUser.user_password == password.text) {
+                        PlayerPrefs.SetString("user_name", shortUser.user_name);
+                        SceneManager.LoadScene("Intro");
+                    } else {
+                        errorMessage.SetActive(true);
+                        errorMessage.GetComponent<TMP_Text>().text = "Incorrect username or password!";
+                    }
+                }
+                else
+                {
+                    errorMessage.SetActive(true);
+                    errorMessage.GetComponent<TMP_Text>().text = "User does not exist!";
+                }
+            } else {
+                errorMessage.SetActive(true);
+                errorMessage.GetComponent<TMP_Text>().text = "Error! Please try again.";
+            }
+        }
+    }
+}
