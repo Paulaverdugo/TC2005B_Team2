fa35ae74e84d8e9409126686c6bdfa640027cd84
Paula Verdugo <paulaverdugo@Paulas-MacBook-Pro-2.local>
2023-06-05 09:47:07 -0600

    Perdon Mariel

---
+++ Web/ShadowCollective/Back/const.js
@@ -0,0 +1,17 @@
+
+export const PROD = {
+    host: "scollectivedb.caqxtnetpk9j.us-east-2.rds.amazonaws.com",
+    user: "scollective",
+    password: "scollectivemaster",
+    database: "scollective",
+ };
+
+export const DEV = {
+    host: "localhost",
+    user: "scollective",
+    password: "Micosis_22",
+    database: "scollective",
+ };
+
+export const ENV = DEV;
+export const PORT = 8000;
