d66cfeb93b4e250de10ba4a8c58790d13566f6e2
coldhed <santiago.rdgz2001@gmail.com>
2023-06-08 13:41:18 -0600

    Added a death scene

---
+++ Videojuego/Shadow Collective/Assets/Scripts/Misc/DeathSceneController.cs
@@ -0,0 +1,34 @@
+/*
+    Script to control the player's input in the death scene
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathSceneController : MonoBehaviour
+{
+    [SerializeField] private GameObject canvas;
+    private bool isCanvasActive = false;
+
+    void Start()
+    {
+        StartCoroutine(ShowCanvas());
+    }
+
+    void Update()
+    {
+        if (Input.anyKey && isCanvasActive)
+        {
+            // go to the level one
+            SceneManager.LoadScene("Level1");
+        }
+    }
+
+    private IEnumerator ShowCanvas()
+    {
+        yield return new WaitForSeconds(1f);
+        canvas.SetActive(true);
+    }
+}
