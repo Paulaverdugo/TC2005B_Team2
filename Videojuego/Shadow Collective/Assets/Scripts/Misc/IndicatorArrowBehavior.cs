ff89f85a639e09ca25185b7b5f146791b9d23ba6
coldhed <santiago.rdgz2001@gmail.com>
2023-06-09 17:28:18 -0600

    Changed the script to base the calculations from the player's position instead of the arrow's

--- Videojuego/Shadow Collective/Assets/Scripts/Misc/IndicatorArrowBehavior.cs
+++ Videojuego/Shadow Collective/Assets/Scripts/Misc/IndicatorArrowBehavior.cs
@@ -9,6 +9,7 @@ using UnityEngine;
 public class IndicatorArrowBehavior : MonoBehaviour
 {
     [SerializeField] private GameObject levelEnd;
+    [SerializeField] private GameObject player;
     private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
@@ -27,7 +28,7 @@ public class IndicatorArrowBehavior : MonoBehaviour
         }
 
         // point the arrow towards the level end
-        Vector3 direction = levelEnd.transform.position - gameObject.transform.position;
+        Vector3 direction = levelEnd.transform.position - player.transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         gameObject.transform.rotation = Quaternion.Euler(0f, 0f, angle);
