220612594e743e4220bb181cc34714a5e16f50c0
Tarazonaa <andres.tara.so@gmail.com>
2023-06-14 14:53:05 -0600

    Add proxy settings (hopefully they work)

--- Web/ShadowCollective/Back/index.js
+++ Web/ShadowCollective/Back/index.js
@@ -10,10 +10,34 @@ import {
 import cors from "cors";
 
 import mysql from "mysql2/promise";
-import {ENV, PORT} from "./const.js";
+import { ENV, PORT } from "./const.js";
 
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+import "dotenv/config"
+
+const API_ENDPOINT = process.env.API_ENDPOINT;
 
 const app = express();
+
+const options = {
+    target: API_ENDPOINT, // target host
+    changeOrigin: true, // needed for virtual hosted sites
+    pathRewrite: {
+        [`^/api/users`]: '/users', // rewrite path 
+        [`^/api/stats`]: '/stats', // rewrite path 
+        [`^/api/progress`]: '/progress', // rewrite path 
+        [`^/api/event`]: '/event', // rewrite path 
+        [`^/api/gadget`]: '/gadget', // rewrite path 
+    },
+}
+
+// extend the proxy depending on the endpoint
+app.use('/api/users', createProxyMiddleware(options), usersRouter);
+app.use('/api/stats', createProxyMiddleware(options), statsRouter);
+app.use('/api/progress', createProxyMiddleware(options), progressRouter);
+app.use('/api/event', createProxyMiddleware(options), eventRouter);
+app.use('/api/gadget', createProxyMiddleware(options), gadgetRouter);
 
 app.use(express.json());
 app.use(cors());
@@ -22,13 +46,6 @@ app.use(cors());
 app.get("/", (req, res) => {
     res.send(`Lo siento Octavio ya no tenemos el puerto expuesto 😈`);
 });
-
-// ----  Rutas ---- 
-app.use("/users", usersRouter);
-app.use("/stats", statsRouter);
-app.use("/progress", progressRouter);
-app.use("/event", eventRouter);
-app.use("/gadget", gadgetRouter);
 
 // ----- Body Parser -----
 app.use(bodyParser.json());
